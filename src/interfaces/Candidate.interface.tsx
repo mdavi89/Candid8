0ed2a923dc13281f6c4f08391ba6344d11c302c2
Matthew Davis <mdavi359@yahoo.com>
2024-11-20 13:25:34 -0500

    Added Candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    login: string,
+    avatar_url: string,
+    name: string,
+    email: string,
+    company: string,
+    location: string,
+    bio: string,
+}
+
+export default Candidate;

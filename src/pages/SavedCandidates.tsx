193ad436e49f57191e6eeb3e19fd5243415998b1
Matthew Davis <mdavi359@yahoo.com>
2024-11-24 14:04:54 -0500

    Finished touching up SavedCandidates

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -6,14 +6,39 @@ const SavedCandidates: React.FC = () => {
 
   return (
     <div>
-      <h1>Candidate List</h1>
-      <ul>
-        {candidates.map((candidate: Candidate) => (
-          <li key={candidate.login}>
-            {candidate.name || candidate.login} ({candidate.location || 'N/A'})
-          </li>
-        ))}
-      </ul>
+      <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">Avatar</th>
+      <th scope="col">Login</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Company</th>
+      <th scope="col">Location</th>
+      <th scope="col">Bio</th>
+    </tr>
+  </thead>
+  <tbody>
+  {candidates.map((candidate: Candidate) => (
+    <tr key={candidate.login}>
+      <td>
+        <img className="avatar" src={candidate.avatar_url} alt="avatar"style={{
+      width: 50, // Set max width
+      height: 50, // Set max height
+      objectFit: 'cover', // Ensures the image fits the area
+      borderRadius: '50%', // Makes it circular
+    }} />
+      </td>
+      <td>{candidate.login}</td>
+      <td>{candidate.name}</td>
+      <td>{candidate.email}</td>
+      <td>{candidate.company}</td>
+      <td>{candidate.location}</td>
+      <td>{candidate.bio}</td>
+    </tr>
+  ))}
+  </tbody>
+</table>
     </div>
   );
 };
